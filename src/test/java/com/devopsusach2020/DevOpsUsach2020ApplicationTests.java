6eaeeb7652a53e9a23f8602e44405d50bab298b5
Jorge Parra <jorge.parra@karibu.cl>
2020-11-30 23:22:24 -0300

    [feature/sonar] Pruebas unitarias del metodo getData

--- src/test/java/com/devopsusach2020/DevOpsUsach2020ApplicationTests.java
+++ src/test/java/com/devopsusach2020/DevOpsUsach2020ApplicationTests.java
@@ -8,6 +8,8 @@ class DevOpsUsach2020ApplicationTests {
 
 	@Test
 	void contextLoads() {
+		String[] args = {};
+		DevOpsUsach2020Application.main(args);
 	}
 
 }
---
+++ src/test/java/com/devopsusach2020/rest/RestDataTest.java
@@ -0,0 +1,41 @@
+package com.devopsusach2020.rest;
+
+import com.devopsusach2020.model.Pais;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class RestDataTest {
+
+    @Test
+	void getDataWithMessage() {
+        RestData target = new RestData();
+        String message = "Prueba de Ejemplo";
+        Pais output = target.getData(message);
+        Assertions.assertNotNull(output);
+        Assertions.assertNotNull(output.getMensaje());
+        Assertions.assertTrue(output.getMensaje().equals("Mensaje Recibido: " + message));
+    }
+
+    @Test
+	void getDataWithMessageEmpty() {
+        RestData target = new RestData();
+        String message = "";
+        Pais output = target.getData(message);
+        Assertions.assertNotNull(output);
+        Assertions.assertNotNull(output.getMensaje());
+        Assertions.assertTrue(output.getMensaje().equals("Mensaje Recibido: "));
+    }
+
+    @Test
+	void getDataWithMessageNull() {
+        RestData target = new RestData();
+        Pais output = target.getData(null);
+        Assertions.assertNotNull(output);
+        Assertions.assertNotNull(output.getMensaje());
+        Assertions.assertTrue(output.getMensaje().equals("Mensaje Recibido: null"));
+    }
+
+}
